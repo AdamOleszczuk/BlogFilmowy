1a4e0dd5d6109564cc3567df25674276f0cab970
Adam Oleszczuk <adam.oleszczuk07@gmail.com>
2023-02-25 23:37:07 +0100

    zmiana ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-d(+ni12j#n$jiw4_56l&sq7=!r*$=jk!n6un-ifx$jm@xv2l8a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','adam777pythonanywhere.com']
 
 
 # Application definition
