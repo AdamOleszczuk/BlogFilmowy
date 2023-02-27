eb2b9ef9f6ed00c8282a543b3ee3690e786ff3de
Adam Oleszczuk <adam.oleszczuk07@gmail.com>
2023-02-27 20:39:40 +0100

    Dodanie formularzy

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from .models import Post
 
-class ImgForm(forms.ModelForm):
+class PostForm(forms.ModelForm):
+
+    title = forms.CharField(help_text='maksymalnie 200 znaków')
+
+
     class Meta:
         model = Post
-        fields = ['image']
+        fields = ['title', 'text', 'image']
 
-class ImgForm:
-    pass
+
+# Klasa Meta przekazuje Django informacje o tym jaki model powinien
+# być wykorzystany do stworzenia tego formualarza
+# oraz jakie pola powinny pojawić się w formularzu
