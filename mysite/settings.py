430dd198b72d33ac266cebb3e6e95ed958f4b5b6
Adam Oleszczuk <adam.oleszczuk07@gmail.com>
2023-03-07 10:52:59 +0100

    dodanie 'djangocms_admin_style'

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com','adam777.pythonanywhere.com', 
 # Application definition
 
 INSTALLED_APPS = [
+    'djangocms_admin_style'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
